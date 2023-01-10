fe58d1b660d348345b465a21c4e8e314f2f5aaac
lee-gilro <rlfgussh@gmail.com>
2023-01-10 13:45:51 +0900

    first commit

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Announcement, Faq
 
 admin.site.register(Profile)
+admin.site.register(Announcement)
+admin.site.register(Faq)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.utils import timezone
+from django.urls import reverse
 
 
 # Extending User Model Using a One-To-One Link
@@ -23,3 +25,16 @@ class Profile(models.Model):
             new_img = (100, 100)
             img.thumbnail(new_img)
             img.save(self.avatar.path)
+
+class Announcement(models.Model):
+    title = models.CharField(max_length= 100)
+    date = models.DateTimeField()
+    content = models.TextField()
+    def __str__(self):
+        return self.title
+
+class Faq(models.Model):
+    question = models.CharField(max_length= 200)
+    answer = models.TextField()
+    def __str__(self) -> str:
+        return self.question
